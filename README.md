4016e9475740e837ceb778f2108f1f4aa223a092
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-02-14 16:52:57 +0300

    Rock paper scissors game implemented

---
+++ README.md
@@ -0,0 +1,36 @@
+# Rock Paper Scissors Game
+
+## About
+
+This Rock-Paper-Scissors Game is a fun, interactive web application built with React.js and styled using Tailwind CSS. It allows users to play the classic game against an automated opponent, with the computer's moves being randomly generated. This project is perfect for developers looking to practice their React skills, including state management with useState, implementing game logic, and creating responsive designs.
+
+## Features
+
+- **Game Play**: Engage in the classic Rock-Paper-Scissors game against the computer.
+- **Score Tracking**: Real-time tracking of player and computer scores.
+- **Recent Moves Display**: Shows the last three moves of both the player and the computer.
+- **Responsive Design**: Utilizes Tailwind CSS for a responsive layout that looks good on any device.
+- **Dynamic Message Updates**: Displays win, lose, or tie messages dynamically based on the game's outcome.
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js
+- Git
+
+### Installation
+
+- Clone the repo:
+  `git clone https://github.com/doguozcan/rock-paper-scissors.git`
+- Navigate to the project directory:
+  `cd rock-paper-scissors`
+- Install npm packages
+  `npm install`
+- Start the application
+  `npm start`
+- Visit <a href="http://localhost:3000">localhost</a> to view the app in your browser.
+
+## Live Preview
+
+- You can visit <a href="https://doguozcan.github.io/rock-paper-scissors/">the live preview</a>
---
+++ src/App.js
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+
+export default function App() {
+  const [myMoves, setMyMoves] = useState([])
+  const [computersMoves, setComputersMoves] = useState([])
+  const [result, setResult] = useState([0, 0])
+  const [message, setMessage] = useState('')
+
+  const status = (myMove, computersMove) => {
+    if (myMove === computersMove) {
+      return 0
+    } else if (
+      (myMove === '👊' && computersMove === '✌️') ||
+      (myMove === '✋' && computersMove === '👊') ||
+      (myMove === '✌️' && computersMove === '✋')
+    ) {
+      return 1
+    } else {
+      return -1
+    }
+  }
+
+  const play = (myMove) => {
+    const choices = ['👊', '✋', '✌️']
+    const computersMove = choices[Math.floor(Math.random() * 3)]
+    const gameResult = status(myMove, computersMove)
+
+    if (gameResult === 1) {
+      setResult([result[0] + 1, result[1]])
+      setMessage('You win 😺')
+    } else if (gameResult === 0) {
+      setMessage('Tie 😼')
+    } else {
+      setResult([result[0], result[1] + 1])
+      setMessage('You lost 😾')
+    }
+
+    setMyMoves([...myMoves, myMove])
+    setComputersMoves([...computersMoves, computersMove])
+  }
+
+  return (
+    <div className="flex flex-col items-center m-12 h-screen font-montserrat">
+      <div className="w-1/2 max-w-sm ">
+        <p className="text-3xl sm:text-5xl text-white text-center">
+          {result[0]} - {result[1]}
+        </p>
+        <div className="text-3xl sm:text-5xl flex justify-center p-5 m-5">
+          <button
+            className="hover:scale-125 duration-200 opacity-80 hover:opacity-100"
+            onClick={() => play('👊')}
+          >
+            👊
+          </button>
+          <button
+            className="hover:scale-125 duration-200 opacity-80 hover:opacity-100"
+            onClick={() => play('✋')}
+          >
+            ✋
+          </button>
+          <button
+            className="hover:scale-125 duration-200 opacity-80 hover:opacity-100"
+            onClick={() => play('✌️')}
+          >
+            ✌️
+          </button>
+        </div>
+        <p className="text-3xl sm:text-5xl text-white text-center m-5 p-5 duration-150">
+          {message}
+        </p>
+        <div className="flex flex-col md:flex-row sm:justify-between text-white">
+          <div className="text-center">
+            <p className="mb-4 text-3xl">You</p>
+            {myMoves.slice(-3).map((play, index) => (
+              <p key={index} className="text-3xl">
+                {play}
+              </p>
+            ))}
+          </div>
+          <div className="text-center">
+            <p className="mb-4 text-3xl">Computer</p>
+            {computersMoves.slice(-3).map((play, index) => (
+              <p key={index} className="text-3xl">
+                {play}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
